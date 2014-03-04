666e13b3732167785e1e5d4dded6c00ccf58f2a7
Seye Ojumu <sojumu@seyes-mbp.lan>
2014-03-04 17:43:25 -0600

    iadded some listing shit.

---
+++ creators/flights.js
@@ -0,0 +1,66 @@
+var flights = {};
+var debug = require('debug')('flights');
+var uuid = require('uuid').v4;
+
+flights.findById = function (id, callback) {
+    debug('findById:', id);
+    members.findOne({id: id}, function (err, member) {
+        if (member && member._id)
+            delete member._id;
+        callback(err, member);
+    });
+};
+
+flights.trip = {
+
+	id: '3c11ebf2-9376-4f7f-9aa0-1b62f0c71ba8',
+
+	airline: 'AA',
+
+	from: 'DFW',
+	to: 'LGA',
+
+	segments: [
+		{
+
+			depart: '2014-03-05T18:06',
+			arrive: '2014-03-05T20:00',
+
+			from: 'DFW',
+			to: 'LGA',
+
+			flight_number: '123'
+
+		}
+	],
+
+	capacity:  160,
+	available:  20,
+
+	listed: []
+
+};
+
+function book (flight) {
+	// go look up the flight & update the booked
+	// sort the list
+}
+
+function list (flight, employee) {
+
+}
+
+function list (flight, member) {
+
+}
+
+function routes () {
+	// 
+	return {
+		AA: {
+			DFW: ['LGA']
+		}
+	};
+}
+
+module.exports = flights;
