99f61f58c8e56ac7f297286b1fd2026a0a363a5a
Seye Ojumu <sojumu@seyes-mbp.lan>
2014-03-04 18:47:44 -0600

    listings.

--- models/listing.js
+++ models/listing.js
@@ -3,7 +3,7 @@ var listings = DB.collection('listings')
 var debug = require('debug')('listings')
 var _        = require('underscore');
     _.str    = require('underscore.string');
-var createPassword = require('password-generator');
+var xPassword = require('password-generator');
 var bcrypt = require('bcrypt');
 
 listings.ensureIndex({email: 1}, {unique: true, dropDups: true}, function () { });
@@ -51,49 +51,17 @@ function generate_password (callback) {
 
 listings.create = function (listing, callback) {
 
-	listing = listing || {};
-	// callback(null, {});
+    listing = listing || {};
+    // callback(null, {});
 
-    // listing = _.defaults(listing, {
-    //     // fullName: null,
-    //     // shortBio: null,
-    //     // fullBio: null,
-    //     // picture: null,
-    //     // links: [],
-    //     // website: null,
-    //     // projects: [], // array of project document _ids
-    //     // airline: null,
-    // });
+    listing = _.defaults(listing, {
 
-
-    listings.insert(listing, {safe: true}, function (err, listing) {
-    	callback(err, listing);
     });
 
 
-    // verify_listing(listing, function (err, verification) {
-    //     if (verification.invalid)
-    //         return callback(new Error(verification.reason));
-    //     generate_password(function (err, password_info) {
-    //         if (err)
-    //             return callback(err);
-
-    //         listing.password = password_info.hash;
-    //         listings.insert(listing, {safe: true}, function (err, listing) {
-    //             // if (err)
-    //             //     console.log(err.message, err.stack);
-    //             if (err && err.code == 11000)
-    //                 err = new Error('That email address has already registered');
-    //             if (err)
-    //                 return callback(err);
-    //             listing = listing[0];
-    //             listing.password = password_info.password;
-    //             // console.log(listing);
-    //             callback(err, listing);
-    //         });
-
-    //     });
-    // });
+    listings.insert(listing, {safe: true}, function (err, listing) {
+        callback(err, listing);
+    });
 
 }
 
